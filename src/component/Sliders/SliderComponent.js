bfee2dc1b58916cdb52c5e4fa713a40679e2974f
NetbixTechnology <134360436+NetbixTechnology@users.noreply.github.com>
2023-07-13 21:28:37 +0530

    second push

--- src/component/Sliders/SliderComponent.js
+++ src/component/Sliders/SliderComponent.js
@@ -5,7 +5,7 @@ const SliderComponent = ({ details }) => {
     return (
         <div className=' bg-[#f2f146] mx-4 rounded-lg spider_slider'>
             <div className='px-2 py-6 text-center text-black'>
-                <img src={details.cover_image} alt="img_spidy" className='w-[150px] h-[200px] object-contain mx-auto' />
+                <img src={details.cover_image} alt="img_spidy" className='w-[auto] h-[200px] object-contain mx-auto' />
                 <div className='pt-6 text-2xl'>{details.name}</div>
                 <Link to={'variation/' + details.link} className=' inline-block px-2 py-2 mt-4 rounded-sm about_btn transition-all ease-linear'>See About Me</Link>
             </div>
--- src/component/database/database.js
+++ src/component/database/database.js
@@ -1,7 +1,18 @@
 import pp1 from '../../assets/images/pp1.png';
 import pp2 from '../../assets/images/pp2.png';
 import pp3 from '../../assets/images/pp3.png';
-
+import miles1 from '../../assets/images/miles1.png';
+import miles2 from '../../assets/images/miles2.png';
+import miles3 from '../../assets/images/miles3.png';
+import gw1 from '../../assets/images/gw1.png';
+import gw2 from '../../assets/images/gw2.png';
+import gw3 from '../../assets/images/gw3.png';
+import mo1 from '../../assets/images/mo1.png';
+import mo2 from '../../assets/images/mo2.png';
+import no1 from '../../assets/images/no1.png';
+import no2 from '../../assets/images/no2.png';
+import pv1 from '../../assets/images/pv1.png';
+import pv2 from '../../assets/images/pv2.png';
 export const spiderData = [
     {
         "name": "Peter Parker",
@@ -16,8 +27,8 @@ export const spiderData = [
         "name": "Miles Morales",
         "age": 19,
         "link": "milesmorales",
-        "cover_image": pp2,
-        "images": [pp1, pp2, pp3],
+        "cover_image": miles2,
+        "images": [miles1, miles2, miles3],
         "dateOfBirth": "December 15, 2003",
         "superpowers": ["Venom Strike", "Camouflage", "Acrobatics"]
     },
@@ -25,8 +36,8 @@ export const spiderData = [
         "name": "Gwen Stacy",
         "age": 22,
         "link": "gwenstacy",
-        "cover_image": pp3,
-        "images": [pp1, pp2, pp3],
+        "cover_image": gw2,
+        "images": [gw1, gw2, gw3],
         "dateOfBirth": "October 19, 2000",
         "superpowers": ["Spider-Woman abilities", "Spider-Sense", "Web-slinging"]
     },
@@ -34,9 +45,27 @@ export const spiderData = [
         "name": "Miguel O'Hara",
         "age": 30,
         "link": "miguelohara",
-        "cover_image": pp1,
-        "images": [pp1, pp2, pp3],
+        "cover_image": mo1,
+        "images": [mo1, mo2],
         "dateOfBirth": "November 1, 1993",
         "superpowers": ["Talons", "Enhanced vision", "Web-slinging"]
+    },
+    {
+        "name": "Spider-Man Noir",
+        "age": 23,
+        "link": "noir",
+        "cover_image": no1,
+        "images": [no1, no2],
+        "dateOfBirth": "August 10, 1900",
+        "superpowers": ["Enhanced Strength", "Wall-Crawling", "Expert Combatant","Investigative Skills"]
+    },
+    {
+        "name": "Pavitra Prabhakar",
+        "age": 19,
+        "link": "pavitra_prabakar",
+        "cover_image": pv2,
+        "images": [pv1, pv2],
+        "dateOfBirth": "February 20, 2004",
+        "superpowers": ["Enhanced Strength", "Wall-Crawling", "Expert Combatant","Adhesion","Martial Arts Expertise"]
     }
 ]
